c0ceda4083c7e5d1f57a0eab611b6e3e7fcf09cf
gupta8053 <95509697+gupta8053@users.noreply.github.com>
2025-01-02 20:42:30 +0530

    Add files via upload

---
+++ hash_table/two_sum.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashmap;
+        for (int i = 0; i < nums.size(); ++i) {
+            int x = nums[i];
+            int y = target - x;
+            if (hashmap.find(y) != hashmap.end()) {
+                return {hashmap[y], i};
+            }
+            hashmap[x] = i;
+        }
+        return {}; // Return an empty vector if no solution is found
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = solution.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No solution found." << endl;
+    }
+
+    return 0;
+}
